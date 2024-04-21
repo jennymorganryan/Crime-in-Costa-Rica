1a87d6129992dd9a5d549606d3ce93416eef3ee2
jennymorganryan <86384143+jennymorganryan@users.noreply.github.com>
2024-04-20 19:44:16 -0600

    port

--- applicacion/main.py
+++ applicacion/main.py
@@ -91,4 +91,4 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", map=html_map)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=False)

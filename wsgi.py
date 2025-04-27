94d194af50a838ea145fc3ccc6a9848d46ced837
jennymorganryan <86384143+jennymorganryan@users.noreply.github.com>
2025-04-27 12:58:55 -0700

    port mentioned

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from applicacion.build_map import get_map
+import os
 
 app = Flask(__name__, template_folder='applicacion/templates')
 
@@ -9,5 +10,8 @@ def home():
     map_html = m._repr_html_()
     return render_template('index.html', map=map_html)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # VERY IMPORTANT
+    app.run(host="0.0.0.0", port=port, debug=False)
+

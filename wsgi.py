00cd8d35865b223ac2f0b4596e354038b87224b6
jennymorganryan <86384143+jennymorganryan@users.noreply.github.com>
2025-04-27 13:00:23 -0700

    port mentioned

--- wsgi.py
+++ wsgi.py
@@ -12,6 +12,6 @@ def home():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # VERY IMPORTANT
+    port = int(os.environ.get("PORT", 5000))  
     app.run(host="0.0.0.0", port=port, debug=False)
 

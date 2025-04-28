d6d8f6f43895697e3f6d98b508ce2ea3793435c7
jennymorganryan <86384143+jennymorganryan@users.noreply.github.com>
2025-04-27 17:07:11 -0700

    wsgi edits to boot instantly

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,14 @@
-import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    from applicacion.build_map import get_map
+    from applicacion.build_map import get_map  # move this inside
     map_html = get_map()._repr_html_()
     return render_template("index.html", map=map_html)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # dynamic port for Render
+    import os
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
